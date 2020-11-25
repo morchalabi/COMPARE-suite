0ed2940ac05b2757ed62a330c7900d92fccf0f97
Morteza <mor.chalabi@gmail.com>
2020-11-25 13:56:41 +0100

    Update 5_similarity_matrix_generator.R

--- src/functions/5_similarity_matrix_generator.R
+++ src/functions/5_similarity_matrix_generator.R
@@ -7,7 +7,6 @@
 #   cor_ (integer): number of CPU cores like 32
 #   inURL (string): address to input data files like ../data
 #   outURL (string): address to output result like ../out
-
 # Algorithm designed and implemented by Mori C.H., mor.chalabi@gmail.com
 
 require(compaRe)
